6940aa2c901ff5f09d60596560cbb738de7522f9
Ijuaa <pierre.colpart@gmail.com>
2023-11-29 11:43:55 +0100

    finished !

--- src/app.js
+++ src/app.js
@@ -15,6 +15,6 @@ app.post("/api/movies", movieControllers.postMovie);
 app.post("/api/users", userControllers.postUser)
 
 app.put("/api/movies/:id", movieControllers.updateMovie)
-app.put("/api/movies/:id", userControllers.updateUser)
+app.put("/api/users/:id", userControllers.updateUser)
 
 module.exports = app;
